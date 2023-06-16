6438f2b552407b95472a16943202277c80bc2ce0
3bdalah <3bdallah.dev@gmail.com>
2023-06-17 02:36:01 +0300

    update : index.css : - add function comp Friendlist - add function comp friend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="sidebar">
+         <FriendsList />
+      </div>
     </div>
   );
 }
 
+function FriendsList(){
+  const Friends = initialFriends;
+    return (
+      <ul>
+         {Friends.map((friend) => (
+          <Friend friend={friend} key={friend.id}/>
+         ))}
+      </ul>
+    )
+}
+
+
+
+function Friend({ friend }) {
+ 
+  return (
+    <li >
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button> </Button>
+    </li>
+  );
+}
 export default App;
